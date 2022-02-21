17055a3125718b87c2f9941ced6ce892d44f89c3
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-21 19:43:21 +0000

    update every example

--- src/ArrayMethods/__tests__/every.spec.js
+++ src/ArrayMethods/__tests__/every.spec.js
@@ -4,4 +4,10 @@ describe("every", () => {
   it("every examples", () => {
     expect(every([2, 4, 6, 8, 10], (number) => number % 2 === 0)).toBeTruthy();
   });
+
+  it("every example 2", () => {
+    expect(
+      every([1, 2, 4, 6, 8, 10], (number) => number % 2 === 0)
+    ).toBeFalsy();
+  });
 });
