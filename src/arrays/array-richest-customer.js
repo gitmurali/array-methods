7596830e6b30c01c8d34a5bbb0b3faf53ecfe478
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-05 10:25:56 +0100

    solution 2 for richest customer

--- src/arrays/array-richest-customer.js
+++ src/arrays/array-richest-customer.js
@@ -13,3 +13,14 @@ var maximumWealth = function (accounts) {
 
   return result;
 };
+
+// solution #2
+// var res = 0;
+// for(var i =0;i<accounts.length;i++){
+//     var temp = 0;
+//     for(var j = 0;j<accounts[i].length;j++){
+//         temp+=accounts[i][j];
+//     }
+//     res = Math.max(res,temp);
+// }
+// return res;
