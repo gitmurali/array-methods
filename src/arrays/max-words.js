b7e7cfd02c80944fdee247046657b22728063d9d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-05 10:53:14 +0100

    most words found

--- src/arrays/max-words.js
+++ src/arrays/max-words.js
@@ -2,3 +2,14 @@
 2114. Maximum Number of Words Found in Sentences
 https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
  */
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function (sentences) {
+  let max = 0;
+  for (let i = 0; i < sentences.length; i++) {
+    max = Math.max(sentences[i].split(" ").length, max);
+  }
+  return max;
+};
