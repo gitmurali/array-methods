f52920481840e7dc859c3a41c7277397f04eb070
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-06 09:40:36 +0100

    shuffle array 2nd solution

--- src/arrays/array-shuffle.js
+++ src/arrays/array-shuffle.js
@@ -8,9 +8,24 @@ https://leetcode.com/problems/shuffle-the-array/
  * @return {number[]}
  */
 var shuffle = function (nums, n) {
+  // let res=[]
+  // for(let i=0; i<n; i++) {
+  //     res.push(nums[i], nums[i+n]);
+  // }
+  // return res
+
+  let isEvenIndex = true;
   let res = [];
   for (let i = 0; i < n; i++) {
-    res.push(nums[i], nums[i + n]);
+    if (isEvenIndex) {
+      res.push(nums[i]);
+      i--;
+    } else {
+      res.push(nums[i + n]);
+    }
+
+    isEvenIndex = !isEvenIndex;
   }
+
   return res;
 };
