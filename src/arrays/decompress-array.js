9bcb50aaa8cd32c7541c360e57a6531bef13df53
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-09 13:45:27 +0100

    update array value

--- src/arrays/decompress-array.js
+++ src/arrays/decompress-array.js
@@ -6,7 +6,7 @@
 var decompressRLElist = function (nums) {
   return [...nums].reduce((acc, value, i) => {
     if (i % 2 === 0) {
-      acc = [...acc, ...Array(nums[i]).fill(nums[i + 1])];
+      acc = [...acc, ...Array(value).fill(nums[i + 1])];
     }
     return acc;
   }, []);
