53c2e981979f922071b22f699d3ebdf31989756d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-12 13:19:29 +0100

    count matches

---
+++ src/arrays/count-items.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/count-items-matching-a-rule/
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function (items, ruleKey, ruleValue) {
+  const map = {
+    type: 0,
+    color: 1,
+    name: 2
+  };
+  let count = 0;
+  items.forEach((elm) => {
+    elm.forEach((item, idx) => {
+      if (map[ruleKey] === idx && ruleValue === item) {
+        count++;
+      }
+    });
+  });
+  return count;
+};
