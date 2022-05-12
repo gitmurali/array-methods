c6eb8e5afaa6a7eabc73e9429ddd67a4d709a068
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-12 20:20:20 +0100

    count items

--- src/arrays/count-items.js
+++ src/arrays/count-items.js
@@ -21,3 +21,13 @@ var countMatches = function (items, ruleKey, ruleValue) {
   });
   return count;
 };
+
+// solution #2
+// var countMatches = function(items, ruleKey, ruleValue) {
+//   const map = {
+//       type: 0,
+//       color: 1,
+//       name: 2
+//   };
+//   return items.reduce((acc, item, idx) => item[map[ruleKey]] === ruleValue ? acc+1 : acc, 0);
+// };
