32ba5765273c71b45f5fc0673c8e9a96558b9cd4
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-14 22:56:29 +0100

    array chunks using generators

--- src/arrays/array-chunks.js
+++ src/arrays/array-chunks.js
@@ -5,3 +5,13 @@ function chunk(list, chunkSize) {
 }
 
 chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 6);
+
+// solution 2 using generators
+function* chunks2(arr, n) {
+  for (let i = 0; i < arr.length; i += n) {
+    yield arr.slice(i, i + n);
+  }
+}
+
+let someArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log([...chunks2(someArray, 2)]);
