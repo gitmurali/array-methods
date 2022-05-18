7f27446f216c2ea44bd5670982951efd54167dc4
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-18 07:43:01 +0100

    call,apply and bind

--- src/arrays/call-apply-bind.js
+++ src/arrays/call-apply-bind.js
@@ -12,3 +12,10 @@ const anotherPerson = {
 
 console.log(person.print.call(anotherPerson, 1, 2));
 console.log(person.print.apply(anotherPerson, [1, 2]));
+
+function somefunc1(val1, val2) {
+  return this.name + val1 + val2;
+}
+
+const bind = somefunc1.bind(anotherPerson);
+console.log(bind(1, 2));
