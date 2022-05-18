af7b38f308d067701fdf39804cf5a70f8665124a
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-18 07:35:00 +0100

    call function

---
+++ src/arrays/call-apply-bind.js
@@ -0,0 +1,13 @@
+const person = {
+  name: "Lokamn",
+  dob: 12,
+  print: function (value, value2) {
+    console.log(this.dob + value + value2);
+  }
+};
+const anotherPerson = {
+  name: "Pappu",
+  dob: 12
+};
+
+console.log(person.print.call(anotherPerson, 1, 2));
