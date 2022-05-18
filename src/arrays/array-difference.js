3b4c68e0deac1fef867ba431032a77d2b7632fbd
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-18 08:07:02 +0100

    sym diff

--- src/arrays/array-difference.js
+++ src/arrays/array-difference.js
@@ -4,4 +4,10 @@ const diff = (a, b) => {
   return a.filter((v) => !b.includes(v));
 };
 
-console.log(diff([1, 2], [1, 2, 3, 4]));
+//console.log(diff([1, 2], [1, 2, 3, 4]));
+
+const symDiff = (a, b) => {
+  return diff(a, b).concat(diff(b, a));
+};
+
+console.log(symDiff([1, 2], [1, 2, 3, 4]));
