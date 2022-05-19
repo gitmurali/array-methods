aa08a4d18f2e11fb2d5a0b1673cd7454e87da75d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-19 10:17:44 +0100

    remove items from array

--- src/arrays/array-remove.js
+++ src/arrays/array-remove.js
@@ -2,4 +2,11 @@ const remove = (arr, value) => {
   return arr.filter((v) => v !== value);
 };
 
+const remove2 = (arr, val) => {
+  const set = new Set(arr);
+  set.delete(val);
+  return [...set];
+};
+
 console.log(remove(["yahoo", "google", "spotify"], "google"));
+console.log(remove2(["yahoo", "google", "spotify"], "google"));
