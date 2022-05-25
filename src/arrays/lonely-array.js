455ddeaa3c5aa13d8515700a4e6752528fb17da9
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-25 13:06:44 +0530

    lonely array

---
+++ src/arrays/lonely-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findLonely = function (nums) {
+  const map = {};
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      map[nums[i]] += 1;
+    } else {
+      map[nums[i]] = 1;
+    }
+  }
+
+  Object.keys(map).forEach((key) => {
+    if (map[key] === 1 && !map[+key + 1] && !map[+key - 1]) {
+      res.push(key);
+    }
+  });
+
+  return res;
+};
