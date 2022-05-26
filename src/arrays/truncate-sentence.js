9e4494329659b1de0bfe6b6b2cbb937ddf104e81
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-26 18:45:49 +0530

    truncate sentence 3

--- src/arrays/truncate-sentence.js
+++ src/arrays/truncate-sentence.js
@@ -5,8 +5,17 @@
  * @param {number} k
  * @return {string}
  */
-var truncateSentence = function (s, k) {
+const truncateSentence = function (s, k) {
   return s.split(" ").slice(0, k).join(" ");
 };
 
 // const truncateSentence = (s, k) => s.split(' ', k).join(' ');
+
+// ***** simplest approach ********
+// var truncateSentence = function(s, k) {
+//   let spaceCount=0;
+//   for(let i=0; i<s.length; i++) {
+//       if(s[i] === ' ' && ++spaceCount === k) return s.slice(0, i);
+//   }
+//   return s;
+// };
