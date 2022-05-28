8b39b0b6bb4a9b8b2b42c2f29b4c15fdf6ae6eef
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-28 16:14:50 +0530

    using just 1 loop

--- src/arrays/product.diff.js
+++ src/arrays/product.diff.js
@@ -5,7 +5,30 @@
  * @return {number}
  */
 var maxProductDifference = function (nums) {
-  const len = nums.length;
-  nums.sort((a, b) => a - b);
-  return nums[len - 1] * nums[len - 2] - nums[0] * nums[1];
+  // const len = nums.length;
+  // nums.sort((a, b) => a - b)
+  // return (nums[len-1] * nums[len-2]) - (nums[0] * nums[1])
+
+  let min = 1e4,
+    prevMin = min;
+  let max = 0,
+    prevMax = 0;
+
+  for (const n of nums) {
+    if (n > max) {
+      prevMax = max;
+      max = n;
+    } else if (n > prevMax) {
+      prevMax = n;
+    }
+
+    if (n < min) {
+      prevMin = min;
+      min = n;
+    } else if (n < prevMin) {
+      prevMin = n;
+    }
+  }
+
+  return max * prevMax - min * prevMin;
 };
