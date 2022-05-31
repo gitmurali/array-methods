77fde54895647960cf701029dc503fcef4bc4a77
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-31 16:49:43 +0530

    moorse words

--- src/arrays/moorse-words.js
+++ src/arrays/moorse-words.js
@@ -1,1 +1,44 @@
 // https://leetcode.com/problems/unique-morse-code-words/
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function (words) {
+  const alphabet = {
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--.."
+  };
+
+  return new Set(
+    words.map((word) =>
+      word
+        .split("")
+        .map((letter) => alphabet[letter])
+        .join("")
+    )
+  ).size;
+};
