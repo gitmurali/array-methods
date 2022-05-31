c30ff38a6076662b71bd3fa9b09a70178a61bd60
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-05-31 17:01:39 +0530

    flip array and then invert it

--- src/arrays/flip-array.js
+++ src/arrays/flip-array.js
@@ -1,1 +1,9 @@
 //https://leetcode.com/problems/flipping-an-image/
+
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (image) {
+  return image.map((img) => img.reverse().map((elm) => 1 - elm));
+};
