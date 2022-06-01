1b8e689bd2ae12f74eb0370f7ad6a1e4164a5731
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-01 11:43:22 +0530

    find duplicates solution 2

--- src/arrays/find-duplicates.js
+++ src/arrays/find-duplicates.js
@@ -15,3 +15,24 @@ const res = names.reduce((result, cv) => {
 }, []);
 
 console.log([...new Set(res)]);
+
+// solution 2
+
+const findDuplicates = (arr) => {
+  let sorted_arr = arr.slice().sort(); // You can define the comparing function here.
+  // JS by default uses a crappy string compare.
+  // (we use slice to clone the array so the
+  // original array won't be modified)
+  let results = [];
+  for (let i = 0; i < sorted_arr.length - 1; i++) {
+    if (sorted_arr[i + 1] == sorted_arr[i]) {
+      results.push(sorted_arr[i]);
+    }
+  }
+  return results;
+};
+
+let duplicatedArray = [9, 9, 111, 2, 3, 4, 4, 5, 7];
+console.log(
+  `The duplicates in ${duplicatedArray} are ${findDuplicates(duplicatedArray)}`
+);
