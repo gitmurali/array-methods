e8882536b4d991c1ede7fffe26bc34deedee8137
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-01 11:17:25 +0530

    ~index

--- src/arrays/find-replace-by-index.js
+++ src/arrays/find-replace-by-index.js
@@ -6,3 +6,9 @@ var index = items.indexOf(3452);
 if (index !== -1) {
   items[index] = 1010;
 }
+
+//or You can also use the ~ operator if you are into terse JavaScript and want to shorten the -1 comparison:
+
+if (~index) {
+  items[index] = 1010;
+}
