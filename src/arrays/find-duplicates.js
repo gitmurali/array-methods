812ffc4dd3bfbedded2954a2a24abb77625cdad2
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-01 11:38:56 +0530

    find duplicates

--- src/arrays/find-duplicates.js
+++ src/arrays/find-duplicates.js
@@ -1,1 +1,17 @@
 const names = ["Mike", "Matt", "Nancy", "Adam", "Jenny", "Nancy", "Carl"];
+const map = {};
+
+names.forEach((name) => {
+  if (map[name]) {
+    map[name] += 1;
+  } else {
+    map[name] = 1;
+  }
+});
+
+const res = names.reduce((result, cv) => {
+  if (map[cv] > 1) result = [...result, cv];
+  return result;
+}, []);
+
+console.log([...new Set(res)]);
