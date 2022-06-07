e60fcf5ca12f4e0a697fb2b4696ecd2698107cad
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-07 22:14:07 +0530

    move in array

--- src/arrays/array-swap-positions.js
+++ src/arrays/array-swap-positions.js
@@ -5,3 +5,12 @@ const swapPositions = (array, a, b) => {
 
 let array = [1, 2, 3, 4, 5];
 swapPositions(array, 0, 1);
+
+// move
+function move(array, from, to) {
+  array.splice(to, 0, array.splice(from, 1)[0]);
+  return array;
+}
+
+var ar = [1, 2, 3, 4, 5];
+move(ar, 0, 3);
