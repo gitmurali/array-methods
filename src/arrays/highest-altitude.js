a8d78c8645332200cec070aa182115643c210d04
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-11 08:40:40 +0530

    highest altitude

---
+++ src/arrays/highest-altitude.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-the-highest-altitude/submissions/
+
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function (gain) {
+  let alt = [0];
+  let res = 0;
+  for (let i = 1; i <= gain.length; i++) {
+    alt[i] = alt[i - 1] + gain[i - 1];
+    res = alt[i] > res ? alt[i] : res;
+  }
+  return res;
+};
