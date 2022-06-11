a4b39c0912deef33c296f0737fcf0c08d82ffa7d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-11 08:52:58 +0530

    highest altitude using Math.max

--- src/arrays/highest-altitude.js
+++ src/arrays/highest-altitude.js
@@ -6,12 +6,14 @@
  */
 var largestAltitude = function (gain) {
   let alt = [0];
-  let res = 0;
+  // let res = 0;
+
   for (let i = 1; i <= gain.length; i++) {
     alt[i] = alt[i - 1] + gain[i - 1];
-    res = alt[i] > res ? alt[i] : res;
+    // res = alt[i] > res ? alt[i] : res;
   }
-  return res;
+
+  return Math.max(...alt);
 };
 
 const largestAltitude2 = (gain) => {
