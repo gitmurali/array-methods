ede9b7a307cf3342e1b2083322552faab69403dc
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-12 10:18:19 +0530

    solution 2 min array

--- src/arrays/min-array.js
+++ src/arrays/min-array.js
@@ -1,3 +1,6 @@
 const arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4];
 const min = Math.min(...arr);
 console.log(min);
+
+//solution 2
+[14, 58, 20, 77, 66, 82, 42, 67, 42, 4].reduce((a, b) => Math.min(a, b));
