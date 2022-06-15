345640cd2446df41da75bb8355fd39e4d56d7311
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-15 11:57:50 +0530

    range using generator

--- src/arrays/range.js
+++ src/arrays/range.js
@@ -8,3 +8,12 @@ range(0, 8); // [0, 1, 2, 3, 4, 5, 6, 7]
 range(4, 9); // [4, 5, 6, 7, 8]
 range(4, 9, 2); // [4, 6, 8]
 range(4, 9, 3); // [4, 7]
+
+//using generator
+
+function* range(x, y) {
+  while (true) {
+    if (x <= y) yield x++;
+    else return null;
+  }
+}
