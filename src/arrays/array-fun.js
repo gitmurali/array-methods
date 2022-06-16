6ddae828d6337f639eb8327e6ac793cdfb3777fc
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-16 12:05:28 +0530

    array fun2

--- src/arrays/array-fun.js
+++ src/arrays/array-fun.js
@@ -5,3 +5,12 @@ a[[2]] === a[2]; // this is true
 
 var a = { abc: 1 };
 a[["abc"]] === a["abc"]; // this is also true
+
+[0] == false; // true
+if ([0]) {
+  /* executes */
+} // [0] is both true and false!
+
+var a = [0];
+a == a; // true
+a == !a; // also true, WTF?
