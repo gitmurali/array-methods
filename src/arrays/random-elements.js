acb097fdde8882cb9c4c3ee0567d357dcbc2a8b8
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-06-21 21:42:25 +0100

    random elements - no duplicates

--- src/arrays/random-elements.js
+++ src/arrays/random-elements.js
@@ -3,3 +3,23 @@
 
 // random elements 1
 Array.from({ length: 40 }, () => Math.floor(Math.random() * 40));
+
+//random elements - no duplicates
+for (var a = [], i = 0; i < 40; ++i) a[i] = i;
+
+// http://stackoverflow.com/questions/962802#962890
+function shuffle(array) {
+  var tmp,
+    current,
+    top = array.length;
+  if (top)
+    while (--top) {
+      current = Math.floor(Math.random() * (top + 1));
+      tmp = array[current];
+      array[current] = array[top];
+      array[top] = tmp;
+    }
+  return array;
+}
+
+a = shuffle(a);
