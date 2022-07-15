fff87dbe95fd84cab66daf572d29730a61237781
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-15 09:44:34 +0100

    2089. Find Target Indices After Sorting Array

---
+++ src/arrays/target-indices.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function (nums, target) {
+  return nums
+    .sort((a, b) => a - b)
+    .reduce((res, cv, i) => {
+      if (cv === target) res = [...res, i];
+      return res;
+    }, []);
+};
