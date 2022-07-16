6246e3875cdd0d4fe8955cec24cfbdad552c58ca
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-16 17:25:14 +0100

    solution 2 for third max

--- src/arrays/max-number.js
+++ src/arrays/max-number.js
@@ -8,3 +8,20 @@ var thirdMax = function (nums) {
   nums = [...new Set(nums)].sort((a, b) => b - a);
   return nums.length <= 2 ? nums[0] : nums[2];
 };
+
+var thirdMax = function (nums) {
+  let first = -Infinity;
+  let second = -Infinity;
+  let third = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === first || nums[i] === second || nums[i] === third) continue;
+    if (nums[i] > first) {
+      [first, second, third] = [nums[i], first, second];
+    } else if (nums[i] > second) {
+      [second, third] = [nums[i], second];
+    } else if (nums[i] > third) {
+      third = nums[i];
+    }
+  }
+  return third === -Infinity ? first : third;
+};
