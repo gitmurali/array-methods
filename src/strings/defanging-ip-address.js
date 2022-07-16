a189518626f1171d99ce2d35096ae64cea94cc3b
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-16 19:58:53 +0100

    defanging ip address sol 2

--- src/strings/defanging-ip-address.js
+++ src/strings/defanging-ip-address.js
@@ -7,3 +7,20 @@
 var defangIPaddr = function (address) {
   return address.replaceAll(".", "[.]");
 };
+
+//sol 2
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function (address) {
+  let newAddr = "";
+  for (let i = 0; i < address.length; i++) {
+    if (address[i] === ".") {
+      newAddr += "[.]";
+    } else {
+      newAddr += address[i];
+    }
+  }
+  return newAddr;
+};
