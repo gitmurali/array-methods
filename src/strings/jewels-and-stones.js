019a0a1f34ba0a62f6ae0465179f8a05535096fa
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-17 18:31:04 +0100

    solution 1 jewels and stones

--- src/strings/jewels-and-stones.js
+++ src/strings/jewels-and-stones.js
@@ -1,1 +1,25 @@
 // https://leetcode.com/problems/jewels-and-stones/
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+  const map = {};
+  let cnt = 0;
+  for (let i = 0; i < stones.length; i++) {
+    if (map[stones[i]]) {
+      map[stones[i]] += 1;
+    } else {
+      map[stones[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < jewels.length; i++) {
+    if (map[jewels[i]]) {
+      cnt += map[jewels[i]];
+    }
+  }
+
+  return cnt;
+};
