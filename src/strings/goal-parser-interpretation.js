b07a735f7062e3ed2a46f965f2765bb333bd6416
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-17 18:48:16 +0100

    sol 1 interpret

--- src/strings/goal-parser-interpretation.js
+++ src/strings/goal-parser-interpretation.js
@@ -1,1 +1,9 @@
 // https://leetcode.com/problems/goal-parser-interpretation/
+
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function (command) {
+  return command.replaceAll("()", "o").replaceAll("(al)", "al");
+};
