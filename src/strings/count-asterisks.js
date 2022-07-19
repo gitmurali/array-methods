79a8900fa25efc841ffcfe6f0104d2ded0728617
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-19 19:32:43 +0100

    count asterisks

--- src/strings/count-asterisks.js
+++ src/strings/count-asterisks.js
@@ -1,1 +1,20 @@
 // https://leetcode.com/problems/count-asterisks/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function (s) {
+  let green = true;
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (green && s[i] === "*") {
+      count++;
+    }
+    if (s[i] === "|") {
+      green = !green;
+    }
+  }
+
+  return count;
+};
