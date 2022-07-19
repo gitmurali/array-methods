8373ecc496d39bd39d45756f351c9c7a0b4e30d5
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-19 19:44:36 +0100

    number of strings as substring

--- src/strings/number-of-strings-that-appear-as-substrings-in-word.js
+++ src/strings/number-of-strings-that-appear-as-substrings-in-word.js
@@ -1,1 +1,14 @@
 // https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function (patterns, word) {
+  let count = 0;
+  patterns.forEach((cv) => {
+    if (word.includes(cv)) count++;
+  });
+  return count;
+};
