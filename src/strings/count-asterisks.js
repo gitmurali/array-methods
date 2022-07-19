85d165473c8ccc0262cdef0bd4d5f4e666421430
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-19 19:33:32 +0100

    count asterisks solution 2

--- src/strings/count-asterisks.js
+++ src/strings/count-asterisks.js
@@ -18,3 +18,19 @@ var countAsterisks = function (s) {
 
   return count;
 };
+
+// solution 2
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function (s) {
+  let asterisks = 0;
+  s.split("|").forEach((sub, i) => {
+    if (i % 2 === 0) {
+      asterisks += sub.split("").filter((c) => c === "*").length;
+    }
+  });
+
+  return asterisks;
+};
