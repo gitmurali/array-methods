8e9d24c581355714f25f806ecb18c2d28b809f0d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-20 10:51:56 +0100

    reverse word

--- src/strings/reverse-words-in-a-string-iii.js
+++ src/strings/reverse-words-in-a-string-iii.js
@@ -1,1 +1,14 @@
 //https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  const result = s.split(" ").reduce((res, cv) => {
+    res = res + cv.split("").reverse().join("") + " ";
+    return res;
+  }, "");
+
+  return result.trim();
+};
