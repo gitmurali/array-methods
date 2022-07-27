60351244512df9640cb1f3049a8d540dadfa5ac1
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-27 20:41:17 +0100

    DI string match

--- src/strings/di-string-match.js
+++ src/strings/di-string-match.js
@@ -1,1 +1,19 @@
 // https://leetcode.com/problems/di-string-match/
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var diStringMatch = function (s) {
+  let i = 0;
+  let dec = s.length;
+  let inc = 0;
+  let res = [];
+
+  while (res.length !== s.length + 1) {
+    res[i] = s[i] === "D" ? dec-- : inc++;
+    i++;
+  }
+
+  return res;
+};
