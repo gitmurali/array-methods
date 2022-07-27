5dee122cad79584e7feccdf1ef8638b4fed9e6ea
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-27 22:34:26 +0100

    max number of pairs in arrays

--- src/arrays/maximum-number-of-pairs-in-array.js
+++ src/arrays/maximum-number-of-pairs-in-array.js
@@ -1,1 +1,29 @@
 // https://leetcode.com/problems/maximum-number-of-pairs-in-array/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberOfPairs = function (nums) {
+  const map = {};
+  let pairs = 0;
+  let leftOvers = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      map[nums[i]] += 1;
+    } else {
+      map[nums[i]] = 1;
+    }
+  }
+
+  Object.keys(map).forEach((val) => {
+    if (map[val] % 2 === 0) {
+      pairs += map[val] / 2;
+    } else {
+      pairs += Math.floor(map[val] / 2);
+      leftOvers++;
+    }
+  });
+
+  return [pairs, leftOvers];
+};
