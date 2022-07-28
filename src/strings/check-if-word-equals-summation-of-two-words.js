85299888550c88f2cb44f97a3c34081e9858b650
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-28 22:25:14 +0100

    summation of two words

--- src/strings/check-if-word-equals-summation-of-two-words.js
+++ src/strings/check-if-word-equals-summation-of-two-words.js
@@ -1,1 +1,26 @@
 // https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
+
+/**
+ * @param {string} firstWord
+ * @param {string} secondWord
+ * @param {string} targetWord
+ * @return {boolean}
+ */
+var isSumEqual = function (firstWord, secondWord, targetWord) {
+  const sum1 = [...firstWord].reduce((res, cv) => {
+    let code = cv.charCodeAt() - 97;
+    return res + code;
+  }, "");
+
+  const sum2 = [...secondWord].reduce((res, cv) => {
+    let code = cv.charCodeAt() - 97;
+    return res + code;
+  }, "");
+
+  const total = [...targetWord].reduce((res, cv) => {
+    let code = cv.charCodeAt() - 97;
+    return res + code;
+  }, "");
+
+  return Number(sum1) + Number(sum2) === Number(total);
+};
