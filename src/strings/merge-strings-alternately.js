a1fe1727303d1163cec31d83062ae83246987f63
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-28 19:53:46 +0100

    merge alternative string using javascript

--- src/strings/merge-strings-alternately.js
+++ src/strings/merge-strings-alternately.js
@@ -1,1 +1,18 @@
 // https://leetcode.com/problems/merge-strings-alternately/
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+  let i = 0;
+  return Array.from(word1)
+    .reduce((res, cv) => {
+      res = [...res, cv, word2[i]];
+      i++;
+      return res;
+    }, [])
+    .join("")
+    .concat(word2.slice(i));
+};
