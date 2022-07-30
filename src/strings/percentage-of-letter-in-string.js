26ddf4c186db59a7123b8ade490810f54cb2735a
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-07-30 22:46:17 +0100

    solution 3

--- src/strings/percentage-of-letter-in-string.js
+++ src/strings/percentage-of-letter-in-string.js
@@ -16,5 +16,13 @@ var percentageLetter = function (s, letter) {
   return Math.floor((cnt / s.length) * 100);
 };
 
+// solution 2
 const percentageLetter1 = (s, letter) =>
   Math.floor(((s.length - s.replaceAll(letter, "").length) * 100) / s.length);
+
+// solution 3
+var percentageLetter2 = function (s, letter) {
+  let letterFilter = s.split("").filter((char) => char == letter);
+
+  return Math.trunc((letterFilter.length / s.length) * 100);
+};
