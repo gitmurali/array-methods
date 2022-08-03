639043fd69343876ce9103278ca9f3a9d4446311
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-03 20:18:26 +0100

    check if all a's appears before b's

---
+++ src/strings/check-if-all-as-appears-before-all-bs.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkString = function (s) {
+  let bIdx = -1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "b") bIdx = i;
+    if (s[i] === "a" && bIdx >= 0 && i > bIdx) return false;
+  }
+  return true;
+};
