74dc885d3ca71e2b2f7b1daea549b5aedebc314d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-05 23:25:25 +0100

    max typed words

--- src/strings/maximum-number-of-words-you-can-type.js
+++ src/strings/maximum-number-of-words-you-can-type.js
@@ -1,1 +1,13 @@
 // https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function (text, brokenLetters) {
+  let lettersArray = brokenLetters.split("");
+  return text.split(" ").filter((word) => {
+    return !lettersArray.some((letter) => word.includes(letter));
+  }).length;
+};
