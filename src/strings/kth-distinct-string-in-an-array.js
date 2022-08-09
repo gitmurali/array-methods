f2ed3e60da819692a13141f4a82d5addf611f9b3
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-09 22:57:00 +0100

    kth distance in an array

--- src/strings/kth-distinct-string-in-an-array.js
+++ src/strings/kth-distinct-string-in-an-array.js
@@ -1,1 +1,23 @@
 // https://leetcode.com/problems/kth-distinct-string-in-an-array/
+
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function (arr, k) {
+  const map = {};
+  const distinctArr = [];
+  arr.forEach((val) => {
+    if (!map[val]) {
+      map[val] = 1;
+    } else {
+      map[val] += 1;
+    }
+  });
+
+  for (let [key, val] of Object.entries(map))
+    if (val == 1) distinctArr.push(key);
+
+  return distinctArr[k - 1] || "";
+};
