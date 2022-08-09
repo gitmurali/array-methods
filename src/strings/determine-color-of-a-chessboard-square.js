9d84e22dd6cd3b82a2f49db929f635d06f9943c9
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-09 22:04:16 +0100

    chess board color

--- src/strings/determine-color-of-a-chessboard-square.js
+++ src/strings/determine-color-of-a-chessboard-square.js
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/determine-color-of-a-chessboard-square/
+
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+var squareIsWhite = function (coordinates) {
+  let isEven;
+  for (let i = 0; i < coordinates.length; i++) {
+    const val = coordinates[i];
+    if (i === 0) {
+      isEven = val.charCodeAt() % 2 === 0;
+    } else {
+      if (isEven) {
+        return +val % 2 !== 0;
+      } else {
+        return +val % 2 === 0;
+      }
+    }
+  }
+};
