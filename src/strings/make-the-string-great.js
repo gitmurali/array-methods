c9bfae99bb06b7590d415e6919894fc4d3b824b1
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-11 21:18:26 +0100

    using stack

--- src/strings/make-the-string-great.js
+++ src/strings/make-the-string-great.js
@@ -1,1 +1,27 @@
 // https://leetcode.com/problems/make-the-string-great/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function (s) {
+  if (s.length <= 1) {
+    return s;
+  }
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const top = stack[stack.length - 1];
+    console.log(top, s[i]);
+    if (
+      stack.length > 0 &&
+      top.toUpperCase() == s[i].toUpperCase() &&
+      top != s[i]
+    ) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+    console.log(stack);
+  }
+  return stack.join("");
+};
