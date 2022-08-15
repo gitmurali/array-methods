9b630510fb2c3f83d2aa92c4a26373ec106b89bd
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-15 14:55:19 +0100

    min time to type

--- src/strings/minimum-time-to-type-word-using-special-typewriter.js
+++ src/strings/minimum-time-to-type-word-using-special-typewriter.js
@@ -1,1 +1,13 @@
 // https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minTimeToType = function (word, cur = "a") {
+  return [...word].reduce((res, cv) => {
+    const gap = Math.abs(cur.charCodeAt(0) - cv.charCodeAt(0));
+    cur = cv; // assign current value to cur
+    return gap > 13 ? (res += 26 - gap) : (res += gap);
+  }, word.length);
+};
