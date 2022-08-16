10ac578e2b194a89a1e02de80df8d593338a23b3
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-16 18:09:45 +0100

    single pass solution to shortest to char

--- src/strings/shortest-distance-to-a-character.js
+++ src/strings/shortest-distance-to-a-character.js
@@ -20,3 +20,23 @@ var shortestToChar = function (s, c) {
     return [...res, min];
   }, []);
 };
+
+// solution 2
+
+var shortestToChar1 = function (s, c) {
+  const answer = Array(s.length).fill(Infinity);
+  let l = Infinity,
+    r = Infinity;
+
+  for (let f = 0; f < s.length; f++) {
+    let b = s.length - 1 - f;
+
+    l = s[f] === c ? 0 : l + 1;
+    r = s[b] === c ? 0 : r + 1;
+
+    answer[f] = Math.min(answer[f], l);
+    answer[b] = Math.min(answer[b], r);
+  }
+
+  return answer;
+};
