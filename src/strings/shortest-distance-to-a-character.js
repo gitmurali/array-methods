41409bc28b00dc4502294293d33f7ff6c48f72af
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-16 17:28:57 +0100

    solution shortest distance to char

--- src/strings/shortest-distance-to-a-character.js
+++ src/strings/shortest-distance-to-a-character.js
@@ -1,1 +1,22 @@
 // https://leetcode.com/problems/shortest-distance-to-a-character/
+
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+var shortestToChar = function (s, c) {
+  const indices = [];
+  [...s].forEach((val, i) => {
+    if (val === c) indices.push(i);
+  });
+
+  return [...s].reduce((res, cv, i) => {
+    let min = Infinity;
+    indices.forEach((val, j) => {
+      min = Math.min(Math.abs(i - val), min);
+    });
+
+    return [...res, min];
+  }, []);
+};
