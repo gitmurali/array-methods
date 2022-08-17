e6e94893c12a9ede83fba91075441fa115427c0d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-17 11:36:59 +0100

    copy of distinct chars in a given size

--- src/strings/substrings-of-size-three-with-distinct-characters.js
+++ src/strings/substrings-of-size-three-with-distinct-characters.js
@@ -1,1 +1,16 @@
 // https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countGoodSubstrings = function (s) {
+  return [...s].reduce((res, cv, i) => {
+    const copy = s.slice(i, i + 3);
+    if (copy.length === 3) {
+      if (new Set([...copy]).size === 3) {
+        res += 1;
+      }
+    }
+    return res;
+  }, 0);
+};
