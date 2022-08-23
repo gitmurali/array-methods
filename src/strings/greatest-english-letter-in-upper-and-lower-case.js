2b0674e0a9a9ae13faa800fc08af30e6e5ac7616
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-23 10:51:52 +0100

    greatest english letter

---
+++ src/strings/greatest-english-letter-in-upper-and-lower-case.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var greatestLetter = function (s) {
+  let index = 0;
+  [...s].forEach((val, i) => {
+    const code = val.charCodeAt();
+    if (code >= 97 && code <= 122) {
+      const capital = String.fromCharCode(code - 32);
+      if (s.includes(capital)) {
+        if (code > index) {
+          index = code;
+        }
+      }
+    }
+  });
+
+  return index === 0 ? "" : String.fromCharCode(index - 32);
+};
