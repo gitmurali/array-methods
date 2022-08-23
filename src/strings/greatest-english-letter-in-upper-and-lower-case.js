bfb7f9f4895efc94dc3c6a2e536a6f33f1e3f289
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-23 10:55:19 +0100

    solution 2 using set

--- src/strings/greatest-english-letter-in-upper-and-lower-case.js
+++ src/strings/greatest-english-letter-in-upper-and-lower-case.js
@@ -6,7 +6,7 @@
  */
 var greatestLetter = function (s) {
   let index = 0;
-  [...s].forEach((val, i) => {
+  [...s].forEach((val) => {
     const code = val.charCodeAt();
     if (code >= 97 && code <= 122) {
       const capital = String.fromCharCode(code - 32);
@@ -17,4 +17,18 @@ var greatestLetter = function (s) {
   });
 
   return index === 0 ? "" : String.fromCharCode(index - 32);
+};
+
+// solution 2 using set
+var greatestLetter2 = function (s) {
+  let set = new Set(s.split(""));
+  for (let i = 90; i >= 65; i--) {
+    if (
+      set.has(String.fromCharCode(i)) &&
+      set.has(String.fromCharCode(i + 32))
+    ) {
+      return String.fromCharCode(i);
+    }
+  }
+  return "";
 };
