d9bef1d141a92389440339b1a387c041490bc1f4
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-23 13:03:07 +0100

    solution 2 find words

--- src/strings/keyboard-row.js
+++ src/strings/keyboard-row.js
@@ -10,3 +10,19 @@ const findWords = (w) =>
       /^[asdfghjkl]*$/i.test(word) ||
       /^[zxcvbnm]*$/i.test(word)
   );
+
+// solution 2
+const findWords1 = function (words) {
+  return words.filter(function (word) {
+    let row1 = word
+      .split("")
+      .every((c) => "qwertyuiop".indexOf(c.toLowerCase()) > -1);
+    let row2 = word
+      .split("")
+      .every((c) => "asdfghjkl".indexOf(c.toLowerCase()) > -1);
+    let row3 = word
+      .split("")
+      .every((c) => "zxcvbnm".indexOf(c.toLowerCase()) > -1);
+    return row1 || row2 || row3;
+  });
+};
