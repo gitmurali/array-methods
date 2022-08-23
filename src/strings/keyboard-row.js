facb0ff3bb39fee9e8238c6d4590ae207eee1cc3
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-23 13:00:10 +0100

    using regexp filter words

--- src/strings/keyboard-row.js
+++ src/strings/keyboard-row.js
@@ -1,1 +1,12 @@
 // https://leetcode.com/problems/keyboard-row/
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = (w) =>
+  w.filter(
+    (word) =>
+      /^[qwertyuiop]*$/i.test(word) ||
+      /^[asdfghjkl]*$/i.test(word) ||
+      /^[zxcvbnm]*$/i.test(word)
+  );
