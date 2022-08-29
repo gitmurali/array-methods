184375e4414f89702dc606d45af8bd358fbfbf54
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-29 22:35:08 +0100

    digit sum 1

--- src/strings/calculate-digit-sum-of-a-string.js
+++ src/strings/calculate-digit-sum-of-a-string.js
@@ -1,1 +1,22 @@
 // https://leetcode.com/problems/calculate-digit-sum-of-a-string/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var digitSum = function (s, k) {
+  let j = s;
+
+  while (j.length > k) {
+    let a = "";
+    for (let i = 0; i < j.length; i += 3) {
+      const group = j.slice(i, i + k);
+      const res = [...group].reduce((res, cv) => res + +cv, 0);
+      a += res;
+    }
+    j = a;
+  }
+
+  return j;
+};
