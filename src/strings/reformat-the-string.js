e7727eaf17394d8b2265435628441c326c51a27b
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-29 20:34:43 +0100

    reformat the string solution 2

--- src/strings/reformat-the-string.js
+++ src/strings/reformat-the-string.js
@@ -1,3 +1,24 @@
+// https://leetcode.com/problems/reformat-the-string/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reformat1 = function (s) {
+  let a = [];
+  let b = [];
+  for (const c of s) {
+    isNaN(c) ? a.push(c) : b.push(c);
+  }
+
+  if (a.length < b.length) {
+    [a, b] = [b, a];
+  }
+
+  return a.length - b.length <= 1
+    ? a.map((val, i) => val + (b[i] ? b[i] : "")).join("")
+    : "";
+};
+
 /**
  * @param {string} s
  * @return {string}
@@ -7,6 +28,7 @@ var reformat = function (s) {
   let cnt2 = 0;
   let j = 0;
   let k = 1;
+
   for (let i = 0; i < s.length; i++) {
     const letter = s[i].charCodeAt();
     if (letter >= 97 && letter <= 122) {
@@ -18,17 +40,22 @@ var reformat = function (s) {
 
   if (Math.abs(cnt1 - cnt2) > 1) return "";
 
-  return [...s]
-    .reduce((res, cv) => {
-      const letter = cv.charCodeAt();
-      if (letter >= 97 && letter <= 122) {
-        res[j] = cv;
-        j += 2;
-      } else {
-        res[k] = cv;
-        k += 2;
-      }
-      return res;
-    }, [])
-    .join("");
+  const result = [...s].reduce((res, cv) => {
+    const letter = cv.charCodeAt();
+    if (letter >= 97 && letter <= 122) {
+      res[j] = cv;
+      j += 2;
+    } else {
+      res[k] = cv;
+      k += 2;
+    }
+    return res;
+  }, []);
+
+  if (
+    (isNaN(result[result.length - 1]) && isNaN(result[result.length - 2])) ||
+    (!isNaN(result[result.length - 1]) && !isNaN(result[result.length - 2]))
+  ) {
+    result.unshift(result.pop());
+  }
 };
