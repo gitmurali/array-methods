0bc4884114263a598dc19336f0973dc4b798758b
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-29 22:37:35 +0100

    digit sum 2

--- src/strings/calculate-digit-sum-of-a-string.js
+++ src/strings/calculate-digit-sum-of-a-string.js
@@ -10,7 +10,7 @@ var digitSum = function (s, k) {
 
   while (j.length > k) {
     let a = "";
-    for (let i = 0; i < j.length; i += 3) {
+    for (let i = 0; i < j.length; i += k) {
       const group = j.slice(i, i + k);
       const res = [...group].reduce((res, cv) => res + +cv, 0);
       a += res;
