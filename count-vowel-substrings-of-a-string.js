f0014a33df1bdf5d3cdf5bd535ae8a8159ededdf
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-08-30 10:25:04 +0100

    vowel sub string in a given string using set

--- count-vowel-substrings-of-a-string.js
+++ count-vowel-substrings-of-a-string.js
@@ -24,3 +24,24 @@ var countVowelSubstrings = function (word) {
 
   return total;
 };
+
+// solution 2 using set
+const countVowelSubstrings1 = (word) => {
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  const set = new Set();
+  let count = 0;
+
+  for (let i = 0; i < word.length; i += 1) {
+    set.clear();
+
+    for (let j = 0; j + i < word.length && vowels.has(word[j + i]); j += 1) {
+      set.add(word[i + j]);
+
+      if (set.size === vowels.size) {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
+};
