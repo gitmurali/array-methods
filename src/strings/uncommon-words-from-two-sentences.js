0c6827913b3a76ddd8184282a094c2c6467cd630
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-02 20:42:51 +0100

    sol 1 uncommon words from two sentences.

--- src/strings/uncommon-words-from-two-sentences.js
+++ src/strings/uncommon-words-from-two-sentences.js
@@ -1,1 +1,23 @@
 // https://leetcode.com/problems/uncommon-words-from-two-sentences/
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+var uncommonFromSentences = function (s1, s2) {
+  const arr = [...s1.split(" "), ...s2.split(" ")];
+  const map = {};
+  const result = [];
+  arr.reduce((res, cv) => {
+    map[cv] = (map[cv] ?? 0) + 1;
+    return res;
+  }, []);
+
+  for (const [key, val] of Object.entries(map)) {
+    if (val === 1) {
+      result.push(key);
+    }
+  }
+
+  return result;
+};
