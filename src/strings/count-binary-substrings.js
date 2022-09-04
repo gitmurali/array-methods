cc4a3893e2f3d39d908d91e3ebe72bf3bb772cfa
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-04 20:18:01 +0100

    count binary substrings

--- src/strings/count-binary-substrings.js
+++ src/strings/count-binary-substrings.js
@@ -1,11 +1,23 @@
 // https://leetcode.com/problems/count-binary-substrings/
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var countBinarySubstrings = function (s) {
   let curr = 1,
     prev = 0,
     ans = 0;
-  for (let i = 1; i < s.length; i++)
-    if (s[i] === s[i - 1]) curr++;
-    else (ans += Math.min(curr, prev)), (prev = curr), (curr = 1);
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      curr++;
+    } else {
+      ans += Math.min(curr, prev);
+      prev = curr;
+      curr = 1;
+    }
+    console.log(curr, prev, ans);
+  }
+
   return ans + Math.min(curr, prev);
 };
