b5be6140fe0679152a8947e56b89c8afbcf27c30
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-05 23:25:39 +0100

    partition labels 1

--- src/strings/partition-labels.js
+++ src/strings/partition-labels.js
@@ -1,1 +1,18 @@
 // https://leetcode.com/problems/partition-labels/
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var partitionLabels = function (s) {
+  const res = [];
+  let last = -1;
+  let left = 0;
+  for (let i = 0; i < s.length; i++) {
+    last = Math.max(last, s.lastIndexOf(s[i]));
+    if (i === last) {
+      res.push(i - left + 1);
+      left = i + 1;
+    }
+  }
+  return res;
+};
