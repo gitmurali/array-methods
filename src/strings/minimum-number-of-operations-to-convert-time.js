483f606b04023134c78dc92528fee3cda6a4278c
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-08 15:21:08 +0100

    minimum number of operations to convert time

---
+++ src/strings/minimum-number-of-operations-to-convert-time.js
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-convert-time/
+
+/**
+ * @param {string} current
+ * @param {string} correct
+ * @return {number}
+ */
+var convertTime = function (current, correct) {
+  if (current === correct) return 0;
+
+  let count = 0;
+  let currentInMins =
+    parseInt(current.slice(3)) + parseInt(current.slice(0, 2)) * 60;
+  let correctInMins =
+    parseInt(correct.slice(3)) + parseInt(correct.slice(0, 2)) * 60;
+  let minuteDifference = correctInMins - currentInMins;
+
+  while (minuteDifference !== 0) {
+    if (minuteDifference % 60 === 0) {
+      minuteDifference -= 60;
+      count++;
+    } else if (minuteDifference % 15 === 0) {
+      minuteDifference -= 15;
+      count++;
+    } else if (minuteDifference % 5 === 0) {
+      minuteDifference -= 5;
+      count++;
+    } else {
+      minuteDifference -= 1;
+      count++;
+    }
+  }
+
+  return count;
+};
