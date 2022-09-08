01d7f434bba019b11c20d451a60373f11ddf276f
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-08 15:28:15 +0100

    solution 2 convert time

--- src/strings/minimum-number-of-operations-to-convert-time.js
+++ src/strings/minimum-number-of-operations-to-convert-time.js
@@ -33,3 +33,28 @@ var convertTime = function (current, correct) {
 
   return count;
 };
+
+//solution 2 convert time
+var convertTime = function (current, correct) {
+  // 1. Seperate current and correct into Hours and Minutes
+  const currentHoursMins = current.split(":");
+  const correctHoursMins = correct.split(":");
+
+  // 2. Calculate difference in minutes
+  let minDifference =
+    correctHoursMins[1] -
+    currentHoursMins[1] +
+    (correctHoursMins[0] - currentHoursMins[0]) * 60;
+
+  // 3. Greedily take the highest minutes to lowest minutes
+  let operations = 0;
+  const operationValues = [60, 15, 5, 1]; // this format is maintainable and extensible since other values can easily be added
+  for (const difference of operationValues) {
+    while (minDifference >= difference) {
+      minDifference -= difference;
+      operations++;
+    }
+  }
+
+  return operations;
+};
