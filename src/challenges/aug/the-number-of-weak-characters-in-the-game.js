c9c6f01e1a298282657475e137a43add6afa9779
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-10 22:01:44 +0100

    weak characters

--- src/challenges/aug/the-number-of-weak-characters-in-the-game.js
+++ src/challenges/aug/the-number-of-weak-characters-in-the-game.js
@@ -1,2 +1,16 @@
 // https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
 
+/**
+ * @param {number[][]} properties
+ * @return {number}
+ */
+var numberOfWeakCharacters = function (properties) {
+  properties.sort((a, b) => (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+  let count = 0;
+  let max = 0;
+  for (const arr of properties) {
+    if (arr[1] < max) count++;
+    max = Math.max(arr[1], max);
+  }
+  return count;
+};
