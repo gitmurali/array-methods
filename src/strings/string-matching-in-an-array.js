87b92d3f1230dc507bbce3c2571418d06f486503
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-11 21:44:38 +0100

    string matching

---
+++ src/strings/string-matching-in-an-array.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/string-matching-in-an-array/
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function (words) {
+  const res = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (words[i] !== words[j] && words[i].includes(words[j])) {
+        if (!res.includes(words[j])) res.push(words[j]);
+      }
+    }
+  }
+  return res;
+};
