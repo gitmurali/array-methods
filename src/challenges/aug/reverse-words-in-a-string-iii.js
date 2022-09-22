97c26d32cc3121abdabae628a36bb384c03d1220
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-22 22:31:19 +0100

    solution 1 reverse words

--- src/challenges/aug/reverse-words-in-a-string-iii.js
+++ src/challenges/aug/reverse-words-in-a-string-iii.js
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+// solution 1
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  return s
+    .split(" ")
+    .reduce((res, cv, j) => {
+      return (
+        res +
+        cv.split("").reduce((acc, v, i, arr) => {
+          return acc + arr[arr.length - i - 1];
+        }, "") +
+        " "
+      );
+    }, "")
+    .trim();
+};
