4624c748afdb6f1d9c24ccbdf17860eef8dad25f
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-09-25 19:16:24 +0100

    circular queue using Linked list

--- src/challenges/september/design-circular-queue.js
+++ src/challenges/september/design-circular-queue.js
@@ -69,3 +69,45 @@ MyCircularQueue.prototype.isFull = function () {
  * var param_5 = obj.isEmpty()
  * var param_6 = obj.isFull()
  */
+
+//solution 2 using Linkedlist
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+class MyCircularQueue1 {
+  constructor(k) {
+    this.maxSize = k;
+    this.size = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  enQueue(val) {
+    if (this.isFull()) return false;
+    let newNode = new ListNode(val);
+    if (this.isEmpty()) this.head = this.tail = newNode;
+    else (this.tail.next = newNode), (this.tail = this.tail.next);
+    this.size++;
+    return true;
+  }
+  deQueue() {
+    if (this.isEmpty()) return false;
+    this.head = this.head.next;
+    this.size--;
+    return true;
+  }
+  Front() {
+    return this.isEmpty() ? -1 : this.head.val;
+  }
+  Rear() {
+    return this.isEmpty() ? -1 : this.tail.val;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  isFull() {
+    return this.size === this.maxSize;
+  }
+}
