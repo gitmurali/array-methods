37383129d59c44e6bf5bcdcb2c23f53bbc09358d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-03 18:18:38 +0100

    solution 2 min cost to make rope color ful

--- src/challenges/October/3minimum-time-to-make-rope-colorful.js
+++ src/challenges/October/3minimum-time-to-make-rope-colorful.js
@@ -15,3 +15,17 @@ var minCost = function (colors, neededTime) {
 
   return ans;
 };
+
+// solution 2
+const minCost1 = (s, cost) => {
+  let res = 0;
+  for (let i = 0, max = 0; i < s.length; i++) {
+    res += cost[i];
+    max = Math.max(max, cost[i]);
+    if (s[i] !== s[i + 1]) {
+      res -= max;
+      max = 0;
+    }
+  }
+  return res;
+};
