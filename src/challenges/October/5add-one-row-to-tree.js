b7b574ca39f0dcf01c4db8329386da46ab2c71cd
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-05 13:35:27 +0100

    add one row to tree

--- src/challenges/October/5add-one-row-to-tree.js
+++ src/challenges/October/5add-one-row-to-tree.js
@@ -1,1 +1,27 @@
 // https://leetcode.com/problems/add-one-row-to-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @param {number} depth
+ * @return {TreeNode}
+ */
+var addOneRow = function (root, val, depth) {
+  if (depth === 1) return new TreeNode(val, root, null);
+  if (depth === 2) {
+    root.left = new TreeNode(val, root.left, null);
+    root.right = new TreeNode(val, null, root.right);
+  } else {
+    if (root.left) addOneRow(root.left, val, depth - 1);
+    if (root.right) addOneRow(root.right, val, depth - 1);
+  }
+
+  return root;
+};
