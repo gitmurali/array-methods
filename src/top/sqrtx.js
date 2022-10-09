a8a127b64e2c653a10b7c7fe345fc93f1be748a0
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-09 20:02:51 +0100

    sqrt using binary search.

--- src/top/sqrtx.js
+++ src/top/sqrtx.js
@@ -9,3 +9,26 @@ var mySqrt = function (x) {
   while (result * result <= x) result++;
   return result - 1;
 };
+
+/**
+ * using binary search
+ */
+var mySqrt1 = function (x) {
+  let left = 1;
+  let right = Math.floor(x / 2) + 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
