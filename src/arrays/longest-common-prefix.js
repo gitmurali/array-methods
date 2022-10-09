10f7af94d29ae23573fc9be9f5db058c0e83cd18
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-09 19:02:57 +0100

    remove logs

--- src/arrays/longest-common-prefix.js
+++ src/arrays/longest-common-prefix.js
@@ -7,7 +7,6 @@ var longestCommonPrefix = function (strs) {
   if (!strs) return "";
 
   return strs.reduce((res, cv) => {
-    console.log(res, cv);
     let i = 0;
     while (res[i] && cv[i] && res[i] === cv[i]) i++;
     return cv.slice(0, i);
