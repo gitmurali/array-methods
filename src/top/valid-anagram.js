966b9e80bee1fd61d411b9e178b24882d38a9069
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-12 14:27:18 +0100

    is valid anagram solution 2

--- src/top/valid-anagram.js
+++ src/top/valid-anagram.js
@@ -7,3 +7,16 @@
 var isAnagram = function (s, t) {
   return [...s].sort().join("") === [...t].sort().join("");
 };
+
+/**
+ * solution 2
+ */
+var isAnagram = function (s, t) {
+  const map = {};
+  for (const c of s) map[c] = (map[c] || 0) + 1;
+  for (const k of t) {
+    if (map[k]) map[k] -= 1;
+    else return false;
+  }
+  return Object.values(map).every((v) => !v);
+};
