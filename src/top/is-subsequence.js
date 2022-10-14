4e581837b182175a77a3f0ebea4134de8732d20f
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-14 20:31:46 +0100

    string is subsequence

---
+++ src/top/is-subsequence.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/is-subsequence/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let i = 0;
+  let j = 0;
+  while (j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+  }
+
+  return i === s.length;
+};
