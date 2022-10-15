b603a1bd53b044dc9629a324c94d34bcabbc9aaa
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-15 09:15:57 +0100

    two sum

---
+++ src/top/two-sum.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
