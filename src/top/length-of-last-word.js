42474ed19accb3c7c7feccf7497363637e8a2ac2
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-15 08:41:52 +0100

    length of last word

---
+++ src/top/length-of-last-word.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/length-of-last-word/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  return s.trim().split(" ").pop().length;
+};
