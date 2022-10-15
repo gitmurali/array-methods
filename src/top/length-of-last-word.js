282cb4d4be59ba1f6134a03a019e9a872d973c3f
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-15 08:44:05 +0100

    length of last word

--- src/top/length-of-last-word.js
+++ src/top/length-of-last-word.js
@@ -6,3 +6,20 @@
 var lengthOfLastWord = function (s) {
   return s.trim().split(" ").pop().length;
 };
+
+var lengthOfLastWord = function (s) {
+  return s.split(/[ ]+/).filter(Boolean).at(-1).length;
+};
+
+var lengthOfLastWord = function (s) {
+  let len = 0;
+  let hasStarted = false;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] !== " ") hasStarted = true;
+    if (hasStarted) {
+      if (s[i] === " ") break;
+      len++;
+    }
+  }
+  return len;
+};
