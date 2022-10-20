9725678a588fab4c2fb59224c692d5856a12deed
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-20 14:46:25 +0100

    can place flowers

---
+++ src/top/can-place-flowers.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/can-place-flowers/
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n > 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
+    }
+  }
+
+  return n === 0;
+};
