cb55c12502d4cb9ba1bfffe2509b72555eabecb8
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-20 17:49:55 +0100

    isomorphic string

---
+++ src/top/isomorphic-strings.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/isomorphic-strings/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (!map.has(s[i])) {
+      map.set(s[i], t[i]);
+    } else {
+      if (map.get(s[i]) !== t[i]) return false;
+    }
+  }
+
+  return new Set([...map.values()]).size === map.size;
+};
