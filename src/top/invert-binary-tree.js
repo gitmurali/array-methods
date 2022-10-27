50c168891faabf424d04afbd4db01becf9006db3
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-27 22:51:52 +0100

    invert binary tree 1

--- src/top/invert-binary-tree.js
+++ src/top/invert-binary-tree.js
@@ -4,3 +4,15 @@ var invertTree = function (root) {
   [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
   return root;
 };
+
+var invertTree = function (root) {
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop();
+    if (node !== null) {
+      [node.left, node.right] = [node.right, node.left];
+      stack.push(node.left, node.right);
+    }
+  }
+  return root;
+};
