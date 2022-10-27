125a37ccf8e41dc8265f3fe40d7293fa1b3ab828
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-27 23:06:06 +0100

    reverse string

---
+++ src/top-interview/reverse-string.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/reverse-string/
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
+  return s;
+};
