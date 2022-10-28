4cabfe7d2f6bbb90a753a34a17b70b6be5d3313c
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-10-28 16:53:50 +0100

    max depth of binary tree

---
+++ src/top-interview/maximum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
+};
