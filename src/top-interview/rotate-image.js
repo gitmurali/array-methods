23fec9c3cea25d69ee4cf4654a3c22af7795b4a4
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-11-05 23:39:38 +0000

    matrix swap

--- src/top-interview/rotate-image.js
+++ src/top-interview/rotate-image.js
@@ -6,9 +6,10 @@
 var rotate = function (matrix) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = i; j < matrix[0].length; j++) {
-      let temp = matrix[i][j];
-      matrix[i][j] = matrix[j][i];
-      matrix[j][i] = temp;
+      // let temp = matrix[i][j];
+      // matrix[i][j] = matrix[j][i];
+      // matrix[j][i] = temp;
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
 
