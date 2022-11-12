8cd7e1a7f9675fa495e6703f98fee91e0a989382
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-11-12 22:49:10 +0000

    remove duplicates

---
+++ src/top/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      nums[++i] = nums[j];
+    }
+  }
+
+  return ++i;
+};
