5e632866e33b279b8b9e597f7f4ffaa31194e862
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-12-05 21:50:46 +0000

    merge two linked lists.

---
+++ src/top-interview/merge-two-sorted-lists.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  const head = new ListNode();
+  let node = head;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      node.next = list2;
+      list2 = list2.next;
+    } else {
+      node.next = list1;
+      list1 = list1.next;
+    }
+
+    node = node.next;
+  }
+
+  node.next = list1 || list2;
+  return head.next;
+};
