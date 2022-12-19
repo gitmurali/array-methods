892fb07d1b38c4afb39c7c5d5ee27bf5c3472e2e
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-12-19 21:37:29 +0000

    pre order traversal

---
+++ src/top-interview/binary-tree-preorder-traversal.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  const result = [];
+  function traverse(node) {
+    if (!node) return null;
+    result.push(node.val);
+
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+  }
+
+  traverse(root);
+
+  return result;
+};
