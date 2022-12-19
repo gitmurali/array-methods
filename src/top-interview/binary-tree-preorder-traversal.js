a831caa9e2388bae9e8a7e1d1e6caab859a7560a
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-12-19 21:54:31 +0000

    pre order traversal using stack

--- src/top-interview/binary-tree-preorder-traversal.js
+++ src/top-interview/binary-tree-preorder-traversal.js
@@ -26,3 +26,19 @@ var preorderTraversal = function (root) {
 
   return result;
 };
+
+// stack approach
+var preorderTraversal = function (root) {
+  if (!root) return [];
+  const result = [];
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+
+  return result;
+};
